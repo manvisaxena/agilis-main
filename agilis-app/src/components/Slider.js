a96435c820c43507514ab2e6ea8ec6d4d3ccf4ad
manvisaxena <manvi.maiet@gmail.com>
2022-03-16 15:55:48 +0000

    Adding Next Prev button functionality

--- agilis-app/src/components/Slider.js
+++ agilis-app/src/components/Slider.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-const ImagePanel = styled.div`
+import { ImageLoginUrls, ImageLogoutUrls } from '../resources/ImageUrls';
+
+const ImageContainer = styled.div`
   width: 50%;
   height: 70%;
   min-width: 700px;
@@ -11,12 +14,68 @@ const ImagePanel = styled.div`
   padding: 30px;
   background-color: #9bac9b;
 `;
+const ImagePanel = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 90%;
+`;
+const Img= styled.img`
+  min-width: 150px;
+  min-height: 200px;
+  height: 100%;
+  position: relative;
+
+`;
+const Button= styled.button`
+  background-color: #9bac9b;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  height: 50px;
+`
 const WhiteColorDiv = styled.div`
   color: white;
   margin: 10px 0px;
 `
 
 function Slider() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [allImages, setAllImages] = useState([]);
+  // Clicking next button
+  const nextImageClick = () => {
+    if(!allImages){
+      return;
+    }
+    // increase Index when next clicked else Alert
+    if(allImages.length > currentImageIndex+1){
+      setCurrentImageIndex(currentImageIndex+1);
+    } else {
+      alert('No more images');
+    }
+  }
+
+  // Clicking prev button
+  const prevImageClick = () => {
+    if(!allImages){
+      return;
+    }
+     // decrease Index when prev clicked else Alert
+    if(0 <= currentImageIndex-1){
+      setCurrentImageIndex(currentImageIndex-1);
+    } else {
+      alert('No more images');
+    }
+  }
+
+  useEffect(() => {
+    setAllImages(ImageLoginUrls);
+  }, [])
+
   return (
     <ImagePanel>
       <WhiteColorDiv>
@@ -24,7 +83,12 @@ function Slider() {
           Images
         </b>
       </WhiteColorDiv>
-    </ImagePanel>
+      <ImagePanel>
+        <Button onClick={prevImageClick} >{'<'}</Button>
+        <Img src={allImages[currentImageIndex]} alt="current pic"></Img>
+        <Button onClick={nextImageClick}>{'>'}</Button>
+      </ImagePanel>
+    </ImageContainer>
   )
 }
 
---
+++ agilis-app/src/resources/ImageUrls.js
@@ -0,0 +1,16 @@
+export const ImageLoginUrls = [
+  'https://images.unsplash.com/photo-1646734329499-0afb2db25fb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+  'https://images.unsplash.com/photo-1646673940197-dbf528eea559?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1646496325295-33fcecfbe6c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+]
+
+export const ImageLogoutUrls = [
+  'https://images.unsplash.com/photo-1644140252082-4d1ba5b7cc01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  'https://images.unsplash.com/photo-1644345553435-a8b092362058?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1632387747332-b625584275e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+  'https://media.istockphoto.com/photos/green-turtle-swimming-on-the-great-barrier-reef-queensland-australia-picture-id834997362?s=612x612',
+  'https://media.istockphoto.com/photos/japanese-wildlife-cranes-fox-swan-eagle-deer-picture-id1312451224?s=612x612',
+  'https://media.istockphoto.com/photos/male-lion-is-sitting-on-the-rock-watching-his-land-picture-id864885156',
+  'https://images.unsplash.com/photo-1623880132570-ab1b4297c8c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2535&q=80',
+  'https://images.unsplash.com/photo-1604429837774-dcb40c63c4d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+]
